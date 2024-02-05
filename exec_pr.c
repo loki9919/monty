ee7cb70e595a5d41f9158c4e2251b2c2a041b540
lokmane rouijel <lokmane9919@gmail.com>
2024-02-04 17:41:18 -0800

    monty

--- exec_pr.c
+++ exec_pr.c
@@ -8,21 +8,21 @@
 void (*get_op_func(char *s))(stack_t **, unsigned int)
 {
 	instruction_t _functions[] = {
-		{"push", _push_to_stack},
-		{"pall", _print_all_stack},
-		{"pint", _print_top_stack},
-		{"pop", _remove_top_stack},
-		{"swap", _swap_two_top_stack},
-		{"add", _add_two_top_stack},
-		{"nop", _nop_does_anything},
-		{"sub", _sub_top_second_stack},
-		{"div", _div_second_top_stack},
-		{"mul", _mul_second_top_stack},
-		{"mod", _mod_second_top_stack},
-		{"pchar", _print_char_top_stack},
-		{"pstr", _print_string_top_stack},
-		{"rotl", _rotate_stack_top},
-		{"rotr", _rotate_stack_bottom},
+		{"push", _push_stack},
+		{"pall", printall},
+		{"pint", printtop},
+		{"pop", removetop},
+		{"swap", swaptwotop},
+		{"add", addtwotop},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"pchar", _printchar},
+		{"pstr", _printstring},
+		{"rotl", _rotate},
+		{"rotr", _rotatestack},
 		{NULL, NULL}
 	};
 	int i = 0;
